8c5852a3fd3ace49513adbfb5ec66f04223cbf55
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-05-29 16:20:09 -0700

    Minimally pre functional pkgs profile.

--- doom-pkgs-profile.el
+++ doom-pkgs-profile.el
@@ -29,6 +29,8 @@
 * Relevant Panels:
 ** [[file:/bisos/panels/blee-core/mail/Gnus/_nodeBase_/fullUsagePanel-en.org]]
 * Planned Improvements:
+*
+
 " orgCmntEnd)
 
 ;;;#+BEGIN: blee:bxPanel:foldingSection :outLevel 0 :title "REQUIRES" :extraInfo "Imports"
@@ -37,9 +39,7 @@
 " orgCmntEnd)
 ;;;#+END:
 
-(require 'gnus)
-(require 'gnus-srvr)
-(require 'loop)
+;;(require 'blee-libs)
 
 ;;;#+BEGIN: blee:bxPanel:foldingSection :outLevel 1 :title "Variables And Constants" :extraInfo "defvar, defcustom"
 (orgCmntBegin "
@@ -47,9 +47,9 @@
 " orgCmntEnd)
 ;;;#+END:
 
-(defvar b:profiles:doom:3.00B nil
+(defvar b:profiles:doom:3.0.0.alpha nil
    " #+begin_org
-** =b:profiles:doom:3.00B= list of doom version 3.00B packages and their versions.
+** =b:profiles:doom:3.0.0.alpha= list of doom version 3.0.0.alpha packages and their versions.
 #+end_org "
   )
 
@@ -59,29 +59,178 @@
 " orgCmntEnd)
 ;;;#+END:
 
-;;;#+BEGIN:  b:elisp:defs/defun :defName "b:straight:profile|install"
+;;;#+BEGIN:  b:elisp:defs/defun :defName "b:pkgsProfile|prepare"
 (orgCmntBegin "
-* [[elisp:(show-all)][(>]]  =defun= <<b:straight:profile|install>> [[elisp:(org-shifttab)][<)]] E|
+* [[elisp:(show-all)][(>]]  =defun= <<b:pkgsProfile|prepare>> [[elisp:(org-shifttab)][<)]] E|
 " orgCmntEnd)
-(defun b:straight:profile|install (
+(defun b:pkgsProfile|prepare (
 ;;;#+END:
-                                   profile pkgName)
+                              <pkgsProfileName)
    " #+begin_org
-** DocStr: mua-abstract package version number.
+** DocStr: For now, we only support doom:3.0.0.alpha. *<pkgsProfileName* ignored for now.
+Each pkgs information is stored as a symbol property of b:profiles:doom:3.0.0.alpha.
 #+end_org "
-   (interactive)
-   b:pkg:mua-abstract:version)
+   (let* (
+          ($pkgsListIntake)
+         )
+     (setq $pkgsListIntake (b:read|fromFile "/bisos/blee/profiles/doom/doom-3.0.0-alpha.el"))
+     ;;
+     ;; Next we setup b:profiles:doom:3.0.0.alpha
+     ;;
+     (b:remprop-all 'b:profiles:doom:3.0.0.alpha)
+     (dolist ($each $pkgsListIntake)
+       ;;(print (car $each))
+       (put 'b:profiles:doom:3.0.0.alpha (car $each) (cdr $each)))
+     ;;(symbol-plist 'b:profiles:doom:3.0.0.alpha)
+     ))
+
 
 (orgCmntBegin "
 ** Basic Usage:
 #+BEGIN_SRC emacs-lisp
-(b:pkg:mua-abstract/version)
+(b:pkgsProfile|prepare 'doom:3.0.0.alpha)
 #+END_SRC
 
 #+RESULTS:
-: 0.1
 
 " orgCmntEnd)
+
+;;;#+BEGIN:  b:elisp:defs/defun :defName "b:pkgsProfile:pkg|obtainInfo"
+(orgCmntBegin "
+* [[elisp:(show-all)][(>]]  =defun= <<b:pkgsProfile:pkg|obtainInfo>> [[elisp:(org-shifttab)][<)]] E|
+" orgCmntEnd)
+(defun b:pkgsProfile:pkg|obtainInfo (
+;;;#+END:
+                                     <pkgsProfileName <pkgName)
+   " #+begin_org
+** DocStr: **<pkgsProfileName** ignored for now. Return keyword list for *<pkgName*
+#+end_org "
+   (let* (
+          ($result)
+         )
+     (setq $result
+           (get 'b:profiles:doom:3.0.0.alpha <pkgName))
+     ))
+
+(orgCmntBegin "
+** Basic Usage:
+#+BEGIN_SRC emacs-lisp
+(b:pkgsProfile:pkg|obtainInfo 'doom:3.0.0.alpha 'add-node-modules-path)
+#+END_SRC
+
+#+RESULTS:
+| :repoUrl | https://github.com/codesuki/add-node-modules-path.git | :commit | 5cbdbf0d2015540c59ed8ee0fcf4788effdf75b6 |
+
+" orgCmntEnd)
+
+
+;;;#+BEGIN:  b:elisp:defs/defun :defName "b:pkgsProfile:pkg:straight|install"
+(orgCmntBegin "
+* [[elisp:(show-all)][(>]]  =defun= <<b:pkgsProfile:pkg:straight|install>> [[elisp:(org-shifttab)][<)]] E|
+" orgCmntEnd)
+(defun b:pkgsProfile:pkg:straight|install (
+;;;#+END:
+                                           <pkgsProfileName <pkgName)
+   " #+begin_org
+** DocStr: *<pkgsProfileName* ignored for now. *<pkgName* is installed through straight.
+#+end_org "
+  (let* (
+          ($pkgInfo (b:pkgsProfile:pkg|obtainInfo <pkgsProfileName <pkgName))
+          ($repoUrl (or (plist-get $pkgInfo :repoUrl) "UnSpecified"))
+          ($commit (or (plist-get $pkgInfo :commit) "UnSpecified"))
+         )
+    $commit
+     ))
+
+
+(orgCmntBegin "
+** Basic Usage:
+#+BEGIN_SRC emacs-lisp
+(b:pkgsProfile:pkg:straight|install 'doom:3.0.0.alpha 'add-node-modules-path)
+#+END_SRC
+
+#+RESULTS:
+: 5cbdbf0d2015540c59ed8ee0fcf4788effdf75b6
+
+" orgCmntEnd)
+
+
+;;;#+BEGIN:  b:elisp:defs/defun :defName "b:pkg:straight|profiledInstall"
+(orgCmntBegin "
+* [[elisp:(show-all)][(>]]  =defun= <<b:pkg:straight|profiledInstall>> [[elisp:(org-shifttab)][<)]] E|
+" orgCmntEnd)
+(defun b:pkg:straight|profiledInstall (
+;;;#+END:
+                                       <pkgName)
+   " #+begin_org
+** DocStr: Using the default pkgsProfileName, install *<pkgName* with straight.
+#+end_org "
+   (b:pkgsProfile:pkg:straight|install 'doom:3.0.0.alpha <pkgName)
+   )
+
+
+(orgCmntBegin "
+** Basic Usage:
+#+BEGIN_SRC emacs-lisp
+(b:pkg:straight|profiledInstall 'add-node-modules-path)
+#+END_SRC
+
+#+RESULTS:
+: 5cbdbf0d2015540c59ed8ee0fcf4788effdf75b6
+
+" orgCmntEnd)
+
+
+;;;#+BEGIN:  b:elisp:defs/defun :defName "b:pkg:straight|latestInstall"
+(orgCmntBegin "
+* [[elisp:(show-all)][(>]]  =defun= <<b:pkg:straight|latestInstall>> [[elisp:(org-shifttab)][<)]] E|
+" orgCmntEnd)
+(defun b:pkg:straight|latestInstall (
+;;;#+END:
+                                     <pkgName)
+   " #+begin_org
+** DocStr: Using the default pkgsProfileName, install *<pkgName* with straight.
+#+end_org "
+   (message "NOTYET")
+   )
+
+
+(orgCmntBegin "
+** Basic Usage:
+#+BEGIN_SRC emacs-lisp
+(b:pkg:straight|latestInstall 'add-node-modules-path)
+#+END_SRC
+
+#+RESULTS:
+: NOTYET
+
+" orgCmntEnd)
+
+;;;#+BEGIN:  b:elisp:defs/defun :defName "b:pkg:straight|forDevInstall"
+(orgCmntBegin "
+* [[elisp:(show-all)][(>]]  =defun= <<b:pkg:straight|forDevInstall>> [[elisp:(org-shifttab)][<)]] E|
+" orgCmntEnd)
+(defun b:pkg:straight|forDevInstall (
+;;;#+END:
+                                     <pkgName)
+   " #+begin_org
+** DocStr: Using the default pkgsProfileName, install *<pkgName* with straight.
+#+end_org "
+   (message "NOTYET")
+   )
+
+
+(orgCmntBegin "
+** Basic Usage:
+#+BEGIN_SRC emacs-lisp
+(b:pkg:straight|forDevInstall 'add-node-modules-path)
+#+END_SRC
+
+#+RESULTS:
+: NOTYET
+
+" orgCmntEnd)
+
 
 ;;;#+BEGIN: b:elisp:file/provide :modName nil
 (provide 'doom-pkgs-profile)
